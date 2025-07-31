92a814bee7b4531de1a528e540e4c2f74ff2eba2
balajiyd <124142672+balajiyd@users.noreply.github.com>
2025-07-31 17:56:07 +0530

    Create hasLoop.java

---
+++ hasLoop.java
@@ -0,0 +1,13 @@
+public boolean hasLoop(){
+	    Node fast=head;
+	    Node slow=head;
+	    while(fast != null && fast.next!=null){
+	        slow = slow.next;
+	        fast = fast.next.next;
+	        if(slow == fast){
+	            return true;
+	        }
+	        
+	    }
+	    return false;
+	}
