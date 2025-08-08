e5259c0b2ce4d061570419cf1decf190d9b61cbe
balajiyd <124142672+balajiyd@users.noreply.github.com>
2025-08-08 18:03:07 +0530

    Create insertionSort.java

---
+++ insertionSort.java
@@ -0,0 +1,12 @@
+public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int temp = arr[i];
+            int j = i-1;
+            while(j>-1 && temp<arr[j]){
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+
+
+        }}
