9df42eaac9094d5396438f311020176216bd4249
balajiyd <124142672+balajiyd@users.noreply.github.com>
2025-08-22 15:19:59 +0530

    Create DLL_Constructor.java

---
+++ DLL_Constructor.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Collections.min;
+import static java.util.Collections.swap;
+
+public class Main {
+    public static void main(String[] args) {
+
+DLinkedList myDLL = new DLinkedList(7);
+myDLL.getHead();
+myDLL.gerTail();
+myDLL.getLength();
+myDLL.printlist();
+
+
+
+    }
+
+
+}
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Collections.min;
+import static java.util.Collections.swap;
+
+public class Main {
+    public static void main(String[] args) {
+
+DLinkedList myDLL = new DLinkedList(7);
+myDLL.getHead();
+myDLL.gerTail();
+myDLL.getLength();
+myDLL.printlist();
+
+
+
+    }
+
+
+}
+
+
+
